808dfaa5652ccb2ffbc519470a70dd1aa99de8ec
EdwinSch <edwinschutjes@gmail.com>
2023-03-29 15:52:44 +0200

    add display buttons functionality

--- app.js
+++ app.js
@@ -2,23 +2,34 @@
 import fetchFollowers from "./modules/fetchFollowers.js";
 import displayFollowers from "./modules/displayFollowers.js";
 import paginate from "./modules/paginate.js";
+import displayButtons from "./modules/displayButtons.js";
 
 /* ---- TARGETS && INITIALIZERS ---- */
 const heading = document.querySelector(".section-title h1");
+const btnContainer = document.querySelector(".btn-container");
 
+let index = 0;
+let pages = [];
 /* ---- SCRIPT ---- */
+
+// Setup UI function
+const setupUI = () => {
+  // Call display followers function and pass pages
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
 
 // App initialization function
 const init = async () => {
   // Await fetch API call
   const followers = await fetchFollowers();
-  // Call display followers function and pass followers array
-  displayFollowers(paginate(followers)[0]);
+
   // change 'loading' heading text to new text
   heading.textContent = "pagination";
   // call paginate function
-  const pages = paginate(followers);
-  console.log(pages);
+  pages = paginate(followers);
+  // call setup UI function
+  setupUI();
 };
 
 // On Load: trigger init function
